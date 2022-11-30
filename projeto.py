56e955b03f0866dbf2e4a93cdc190e36954770ee
heber3000 <heberlevy@gmail.com>
2022-11-30 09:44:10 -0300

    Adicionando o método getEntrada, para a entrada de dados

--- projeto.py
+++ projeto.py
@@ -23,6 +23,21 @@ cEntrada = tk.Entry()
 cEntrada.grid(column=1,row=1)
 
 
+
+def getEntrada():
+    dados = EquacaoSegundoGrau(aEntrada.get(),bEntrada.get(),cEntrada.get())
+
+    textArea = tk.Text(master=janela,height=10,width=25)
+    textArea.grid(column=1,row=6)
+    resposta = f'{dados.verificar_equacao()}\n{dados.delta()}'
+    textArea.insert(tk.END,resposta)
+
+    botao = tk.Button(janela,text='Calcular Eq do segundo grau',command=getEntrada,bg='red')
+    botao.grid(column=1,row=6)
+
+
+
+
 class EquacaoSegundoGrau:
     def __init__(self,a,b,c):
         self.a = a
