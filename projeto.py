3cdf277c796b2bc58acb8a2b52c014c893aa9a7d
heber3000 <heberlevy@gmail.com>
2022-11-30 10:49:11 -0300

    Adicionando o método getEntrada, para a entrada de dados

--- projeto.py
+++ projeto.py
@@ -18,22 +18,22 @@ c.grid(column=0,row=3)
 aEntrada = tk.Entry()
 aEntrada.grid(column=1,row=1)
 bEntrada = tk.Entry()
-bEntrada.grid(column=1,row=1)
+bEntrada.grid(column=1,row=2)
 cEntrada = tk.Entry()
-cEntrada.grid(column=1,row=1)
+cEntrada.grid(column=1,row=3)
 
 
 
 def getEntrada():
-    dados = EquacaoSegundoGrau(aEntrada.get(),bEntrada.get(),cEntrada.get())
+    dados = EquacaoSegundoGrau(float(aEntrada.get()),float(bEntrada.get()),float(cEntrada.get()))
 
     textArea = tk.Text(master=janela,height=10,width=25)
     textArea.grid(column=1,row=6)
-    resposta = f'{dados.verificar_equacao()}\n{dados.delta()}'
+    resposta = dados.delta()
     textArea.insert(tk.END,resposta)
 
-    botao = tk.Button(janela,text='Calcular Eq do segundo grau',command=getEntrada,bg='red')
-    botao.grid(column=1,row=6)
+botao = tk.Button(janela,text='Calcular Eq do segundo grau',command=getEntrada,bg='red')
+botao.grid(column=1,row=6)
 
 
 
@@ -44,21 +44,29 @@ class EquacaoSegundoGrau:
         self.b = b
         self.c = c
 
-    def verificar_equacao(self):
-        if self.a == 0:
-            return f'Não é equacao do segundo grau'
-        else:
-            return f'É equação do segundo grau'
 
     def delta(self):
-        self.delta = math.pow(self.b,2) - 4*self.a*self.c
+        self.delta = (math.pow(self.b,2) - 4*self.a*self.c)
         if self.delta < 0:
             return f'Não há raízes'
         elif self.delta == 0:
-            return f'Há apenas uma raiz {(-self.b+math.sqrt(self.delta))/2*self.a}'
+            return f'Só há uma raiz {"%.2f" % float((self.b+math.sqrt(self.delta))/self.a*2)}'
         else:
-            return f'Há duas raízes {(-self.b+math.sqrt(self.delta))/2*self.a} e {(-self.b-math.sqrt(self.delta))/2*self.a}'
+            return f'Há duas raizes {"%.2f" % float((-self.b+math.sqrt(self.delta))/self.a*2)},{"%.2f" % float((-self.b+math.sqrt(self.delta))/self.a*2)}'
 
 
+
+
+
+
+
+
+foto = Image.open("Captura de tela em 2022-11-30 09-54-56.png")
+foto.thumbnail((300,300),Image.ANTIALIAS)
+photo=ImageTk.PhotoImage(foto)
+foto_label = tk.Label(image=photo)
+foto_label.grid(column=1,row=0)
+
+janela.mainloop()
 
 
