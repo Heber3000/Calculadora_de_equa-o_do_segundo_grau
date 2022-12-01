b1597d0706d8ecf18d231c678484ebe0824cf86c
heber3000 <heberlevy@gmail.com>
2022-12-01 09:16:16 -0300

    alterando o método delta

--- projeto.py
+++ projeto.py
@@ -46,19 +46,16 @@ class EquacaoSegundoGrau:
 
 
     def delta(self):
-        self.delta = (math.pow(self.b,2) - 4*self.a*self.c)
-        if self.delta < 0:
-            return f'Não há raízes'
-        elif self.delta == 0:
-            return f'Só há uma raiz {"%.2f" % float((-self.b+math.sqrt(self.delta))/self.a*2)}'
+        if self.a == 0:
+            return f'Não é equação do segundo grau'
         else:
-            return f'Há duas raizes {"%.2f" % float((-self.b+math.sqrt(self.delta))/self.a*2)},{"%.2f" % float((-self.b-math.sqrt(self.delta))/self.a*2)}'
-
-
-
-
-
-
+            self.delta = (math.pow(self.b, 2) - 4 * self.a * self.c)
+            if self.delta < 0:
+                return f'Não há raízes'
+            elif self.delta == 0:
+                return f'Só há uma raiz {"%.2f" % float((-self.b + math.sqrt(self.delta)) / self.a * 2)}'
+            else:
+                return f'Há duas raizes {"%.2f" % float((-self.b + math.sqrt(self.delta)) / self.a * 2)},{"%.2f" % float((-self.b - math.sqrt(self.delta)) / self.a * 2)}'
 
 
 foto = Image.open("Captura de tela em 2022-11-30 09-54-56.png")
